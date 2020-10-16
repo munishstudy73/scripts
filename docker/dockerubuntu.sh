d5199d2470a54765c8eb4d154a40e3fd35c1963c
munishvasu73 <45182462+munishvasu73@users.noreply.github.com>
2020-10-16 16:04:37 +0530

    Update dockerubuntu.sh

--- docker/dockerubuntu.sh
+++ docker/dockerubuntu.sh
@@ -2,14 +2,14 @@
 #Author: Munish
 #Date: 16/10/20
 #Program: Install docker on fresh ubuntu 20.04
+#usage: curl -s https://raw.githubusercontent.com/munishstudy73/scripts/master/docker/dockerubuntu.sh | sh
 
 
-
-sudo apt -y update 
-sudo apt -y install   apt-transport-https ca-certificates curl software-properties-common
+sudo apt  update -y
+sudo apt  install -y   apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt -y update 
+sudo apt  update -y 
 ##apt-cache policy docker-ce (to check if install from docker repo)
-sudo apt -y  install  docker-ce
+sudo apt  install -y  docker-ce
 ##sudo systemctl status docker (check status)
